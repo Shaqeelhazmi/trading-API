b91c29f7c17bf18b04029484c63a34946bb4b5bd
Shaqeel Hazmi Bin Radzifuddin <shaqeel.radzifuddin@mail.utoronto.ca>
2023-10-23 16:20:47 -0400

    Updates BuyUseCaseFactory but alot of errors since other parts not updated yet 23/10

--- src/app/BuyUseCaseFactory.java
+++ src/app/BuyUseCaseFactory.java
@@ -1,2 +1,29 @@
-package app;public class BuyUseCaseFactory {
+package app;
+
+import use_case.buy.BuyDataAccessInterface;
+import use_case.buy.BuyInputBoundary;
+import use_case.buy.BuyInteractor;
+import use_case.buy.BuyOutputBoundary;
+
+import javax.swing.*;
+import java.io.IOException;
+
+public class BuyUseCaseFactory {
+    public static BuyView create(ViewManagerModel viewManagerModel, BuyViewModel buyViewModel, BuyDataAccessInterface userDataAccessObject) {
+    try {
+        BuyController buyController =  createBuyUseCase(viewManagerModel, buyViewModel, userDataAccessObject);
+        return new BuyView(buyController, buyViewModel);
+    } catch (IOException e) {
+        JOptionPane.showMessageDialog(null, "Could not open file");
+    }
+        return null;
+    }
+
+    private static BuyController createBuyUseCase(ViewManagerModel viewManagerModel, BuyViewModel buyViewModel, BuyDataAccessInterface userDataAccessObject) throws IOException{
+        BuyOutputBoundary buyOutputBoundary = new BuyPresenter(viewManagerModel, buyViewModel);
+        UserFactory userFactory = new CommonUserFactory();
+        BuyInputBoundary userBuyInteractor = new BuyInteractor(userDataAccessObject, buyOutputBoundary);
+
+        return new BuyController(userBuyInteractor);
+    }
 }

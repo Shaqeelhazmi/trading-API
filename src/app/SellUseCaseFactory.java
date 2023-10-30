3c2581a199e641757f632fa2e87a9fda8839b86b
Ruochen Feng <ruochen.feng@mail.utoronto.ca>
2023-10-30 14:00:10 -0400

    sellusecasefactory

--- src/app/SellUseCaseFactory.java
+++ src/app/SellUseCaseFactory.java
@@ -1,4 +1,28 @@
 package app;
 
+
+
+import use_case.sell.SellDataAccessInterface;
+
+import javax.swing.*;
+import java.io.IOException;
+
 public class SellUseCaseFactory {
+    public static SellView create(ViewManagerModel viewManagerModel, SellViewModel sellViewModel, SellDataAccessInterface userDataAccessObject) {
+        try {
+            SellController sellController =  createSellUseCase(viewManagerModel, sellViewModel, userDataAccessObject);
+            return new SellView(sellController, sellViewModel);
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(null, "Could not open file");
+        }
+        return null;
+    }
+
+    private static SellController createSellUseCase(ViewManagerModel viewManagerModel, SellViewModel sellViewModel, SellDataAccessInterface userDataAccessObject) throws IOException{
+        SellOutputBoundary sellOutputBoundary = new SellPresenter(viewManagerModel, sellViewModel);
+        UserFactory userFactory = new CommonUserFactory();
+        SellInputBoundary userSellInteractor = new SellInteractor(userDataAccessObject, sellOutputBoundary);
+
+        return new SellController(userSellInteractor);
+    }
 }

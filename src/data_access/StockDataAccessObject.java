b8eced980965cafca8c9749aa5d4b26bba0eae87
Ruochen Feng <ruochen.feng@mail.utoronto.ca>
2023-10-30 12:27:17 -0400

    data_access

--- src/data_access/StockDataAccessObject.java
+++ src/data_access/StockDataAccessObject.java
@@ -5,6 +5,7 @@ import use_case.buy.BuyDataAccessInterface;
 import use_case.sell.SellDataAccessInterface;
 
 import java.io.*;
+import java.time.LocalDateTime;
 import java.util.*;
 
 public class StockDataAccessObject implements BuyDataAccessInterface, SellDataAccessInterface {
@@ -14,12 +15,87 @@ public class StockDataAccessObject implements BuyDataAccessInterface, SellDataAc
 
     private final Map<String, Stock> stocks = new HashMap<>();
 
+    private StockFactory stockFactory;
+
+    public StockDataAccessObject(String csvPath, StockFactory stockFactory) throws IOException {
+        this.stockFactory = stockFactory;
+
+        csvFile = new File(csvPath);
+        //change
+        headers.put("username", 0);
+        headers.put("password", 1);
+        headers.put("creation_time", 2);
+
+        if (csvFile.length() == 0) {
+            save();
+        } else {
+
+            try (BufferedReader reader = new BufferedReader(new FileReader(csvFile))) {
+                String header = reader.readLine();
+
+                // For later: clean this up by creating a new Exception subclass and handling it in the UI.
+                assert header.equals("username,password,creation_time");
+
+                String row;
+                while ((row = reader.readLine()) != null) {
+                    String[] col = row.split(",");
+                    String username = String.valueOf(col[headers.get("username")]);
+                    String password = String.valueOf(col[headers.get("password")]);
+                    String creationTimeText = String.valueOf(col[headers.get("creation_time")]);
+                    LocalDateTime ldt = LocalDateTime.parse(creationTimeText);
+                    Stock stock = stockFactory.create(username, password, ldt);
+                    stocks.put(username, stock);
+                }
+            }
+        }
+    }
+
     public StockDataAccessObject(File csvFile) {
         this.csvFile = csvFile;
     }
 
-    public boolean existsByName(String identifier) {
-        //left
-        return false;
+    @Override
+    public void save(Stock stock) {
+        stocks.put(stock.getName(), stock);
+        this.save();
     }
+
+    @Override
+    public Stock get(String username) {
+        return stocks.get(username);
+    }
+
+    private void save() {
+        BufferedWriter writer;
+        try {
+            writer = new BufferedWriter(new FileWriter(csvFile));
+            writer.write(String.join(",", headers.keySet()));
+            writer.newLine();
+
+            for (Stock stock : stocks.values()) {
+                //change
+                String line = String.format("%s,%s,%s",
+                        stock.getName(), user.getPassword(), user.getCreationTime());
+                writer.write(line);
+                writer.newLine();
+            }
+
+            writer.close();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    /**
+     * Return whether a user exists with username identifier.
+     * @param identifier the username to check.
+     * @return whether a user exists with username identifier
+     */
+    @Override
+    public boolean existsByName(String identifier) {
+        return stocks.containsKey(identifier);
+    }
+
 }

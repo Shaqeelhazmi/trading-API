ba5edb6c64a8d531c582e49987453379613bf316
matkuang <matthew.kuang@mail.utoronto.ca>
2023-11-13 13:47:46 -0500

    Finished implementing use_case for signup and login without front end of code

---
+++ src/entity/CommonUserFactory.java
@@ -0,0 +1,19 @@
+package entity;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+public class CommonUserFactory implements UserFactory {
+    /**
+     * Requires: password is valid.
+     * @param name
+     * @param password
+     * @return
+     */
+
+    @Override
+    public User create(String name, String password, LocalDateTime ltd, ArrayList<Stock> favourites, Portfolio portfolio) {
+
+        return new CommonUser(name, password, ltd, favourites, portfolio);
+    }
+}
--- src/entity/UserFactory.java
+++ src/entity/UserFactory.java
@@ -1,7 +1,8 @@
 package entity;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 
 public interface UserFactory {
-    User create(String username, String password, LocalDateTime ltd);
+    User create(String username, String password, LocalDateTime ltd, ArrayList<Stock> favourites, Portfolio portfolio);
 }

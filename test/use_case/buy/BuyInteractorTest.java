45023d8054cbe8571c34e2d1d93d7ac3e5d2c326
Shaqeel Hazmi Bin Radzifuddin <shaqeel.radzifuddin@mail.utoronto.ca>
2023-11-13 19:13:55 -0500

    BuyUseCaseInteractor

--- test/use_case/buy/BuyInteractorTest.java
+++ test/use_case/buy/BuyInteractorTest.java
@@ -1,13 +1,55 @@
 package use_case.buy;
 
+import entity.PriceHistory;
+import entity.Stock;
 import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class BuyInteractorTest {
 
+    HashMap<String, Double> daily = new HashMap<String, Double>(5);
+
+    HashMap<String, Double> weekly = new HashMap<String, Double>();
+
+    HashMap<String, Double> monthly = new HashMap<String, Double>();
+
+    PriceHistory priceHistory = new PriceHistory(daily, weekly, monthly);
+
+
+    Stock stock = new Stock("TSLA",  priceHistory, "TESLA");
+
+
     @Test
-    void buy() {
+    void prepareSuccessViewTest(){
+        BuyInputData inputData = new BuyInputData(stock, 5);
+        BuyOutputBoundary successPresenter = new BuyOutputBoundary() {
+            @Override
+            public void prepareFailView(String message) {
+                fail("Use case failure unexpected");
+            }
+
+            @Override
+            public void prepareSuccessView(BuyOutputData buyOutputData) {
+                assertEquals(inputData.getStockname(), buyOutputData.getStockBought());
+                assertEquals(inputData.getAmount(), buyOutputData.getAmountStock());
+            }
+        };
+
+    }
+
+    @Test
+    void prepareNotAvailableTest() {
+
+    }
+
+
+    @Test
+    void prepareNotEnoughTest(){
+
     }
 
     @Test

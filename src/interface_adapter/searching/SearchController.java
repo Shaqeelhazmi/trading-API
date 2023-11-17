b39d8fd70a8c4cf3fbde11482371c842a41f023d
Ruochen Feng <ruochen.feng@mail.utoronto.ca>
2023-11-17 14:38:43 -0500

    interface adapter of searching

--- src/interface_adapter/searching/SearchController.java
+++ src/interface_adapter/searching/SearchController.java
@@ -1,4 +1,18 @@
 package interface_adapter.searching;
 
+import entity.Stock;
+import use_case.searching.SearchInputBoundary;
+import use_case.searching.SearchInputData;
+import use_case.searching.SearchInteractor;
+
 public class SearchController {
+    final SearchInputBoundary SearchInteractor;
+
+    public SearchController (SearchInputBoundary searchInteractor){
+        this.SearchInteractor = searchInteractor;
+    }
+    public void search(String stockname){
+        SearchInputData searchInputData = new SearchInputData(stockname);
+        SearchInteractor.search(searchInputData);
+    }
 }

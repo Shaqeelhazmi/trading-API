a09e82f30a4c40ce735b0692ec8e4ba03b6215da
Ruochen Feng <ruochen.feng@mail.utoronto.ca>
2023-11-24 13:59:49 -0500

    searching interface_adpter refines

--- src/interface_adapter/searching/SearchViewModel.java
+++ src/interface_adapter/searching/SearchViewModel.java
@@ -7,6 +7,7 @@ import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
 
 public class SearchViewModel extends ViewModel {
+    public final String TITLE_LABEL = "Search View";
     public static final String Search_Button_Label = "Search";
 
     private SearchState searchState = new SearchState();

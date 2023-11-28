16978fe41d24c5f0bc69577c09efb885cf0e8824
Shaqeel Hazmi Bin Radzifuddin <shaqeel.radzifuddin@mail.utoronto.ca>
2023-11-27 20:12:34 -0500

    Change position of buy and sell methods

--- src/data_access/StockDataAccessObject.java
+++ src/data_access/StockDataAccessObject.java
@@ -1,165 +1,131 @@
-////package data_access;
-////
-////import entity.*;
-////import netscape.javascript.JSException;
-////import netscape.javascript.JSObject;
-////import use_case.buy.BuyDataAccessInterface;
-////import use_case.searching.SearchDataAccessInterface;
-////import use_case.sell.SellDataAccessInterface;
-////
-////import java.io.*;
-////import java.time.LocalDateTime;
-////import java.util.*;
-////
-////public class StockDataAccessObject implements BuyDataAccessInterface, SellDataAccessInterface, SearchDataAccessInterface {
-////    private final JSObject jsonFile;
-////
-////    private final Map<String, Integer> headers = new LinkedHashMap<>();
-////
-////    private final Map<String, Stock> stocks = new HashMap<>();
-////
-////    private StockFactory stockFactory;
-////
-////    public StockDataAccessObject(String JsonPath, StockFactory stockFactory) throws IOException {
-////        this.stockFactory = stockFactory;
-////
-////        jsonFile = new JSObject() {
-////            @Override
-////            public Object call(String methodName, Object... args) throws JSException {
-////                return null;
-////            }
-////
-////            @Override
-////            public Object eval(String s) throws JSException {
-////                return null;
-////            }
-////
-////            @Override
-////            public Object getMember(String name) throws JSException {
-////                return null;
-////            }
-////
-////            @Override
-////            public void setMember(String name, Object value) throws JSException {
-////
-////            }
-////
-////            @Override
-////            public void removeMember(String name) throws JSException {
-////
-////            }
-////
-////            @Override
-////            public Object getSlot(int index) throws JSException {
-////                return null;
-////            }
-////
-////            @Override
-////            public void setSlot(int index, Object value) throws JSException {
-////
-////            }
-////        };
-////        //change
-////        headers.put("stockSymbol", 0);
-////        headers.put("priceHistory", 1);
-////
-////        if (jsonFile.length() == 0) {
-////            save();
-////        } else {
-////
-////            try (BufferedReader reader = new BufferedReader(new FileReader(jsonFile))) {
-////                String header = reader.readLine();
-////
-////                // For later: clean this up by creating a new Exception subclass and handling it in the UI.
-////                assert header.equals("username,password,creation_time");
-////
-////                String row;
-////                while ((row = reader.readLine()) != null) {
-////                    String[] col = row.split(",");
-////                    String stockSymbol = String.valueOf(col[headers.get("username")]);
-////                    PriceHistory priceHistory = None; //change!
-////                    Stock stock = stockFactory.create(stockSymbol, priceHistory);
-////                    stocks.put(stockSymbol, stock);
-////                }
-////            }
-////        }
-////    }
-////
-////    public StockDataAccessObject(File jsonFile) {
-////        this.jsonFile = jsonFile;
-////    }
-////
-////
-////    public void save(Stock stock) {
-////        stocks.put(stock.getStockSymbol(), stock);
-////        this.save();
-////    }
-////
-////
-////    public Stock get(String stockSymbol) {
-////        return stocks.get(stockSymbol);
-////    }
-////
-////    private void save() {
-////        BufferedWriter writer;
-////        try {
-////            writer = new BufferedWriter(new FileWriter(jsonFile));
-////            writer.write(String.join(",", headers.keySet()));
-////            writer.newLine();
-////
-////            for (Stock stock : stocks.values()) {
-////                String line = String.format("%s,%s",
-////                        stock.getStockSymbol(), stock.getPriceHistory());
-////                writer.write(line);
-////                writer.newLine();
-////            }
-////
-////            writer.close();
-////
-////        } catch (IOException e) {
-////            throw new RuntimeException(e);
-////        }
-////    }
-////
-////    public void buy(int amount, Stock stock, CommonUser user, double amount_used_for_purchase){
-//        if (user.getPortfolio().getPortfolio().containsKey(stock.getStockSymbol())) {
-//           int old_value = user.getPortfolio().getPortfolio().get(stock.getStockSymbol());
-//           int new_value = old_value + amount;
-//           user.getPortfolio().getPortfolio().replace(stock.getStockSymbol(), old_value, new_value);
-//           } else {
-//             user.getPortfolio().getPortfolio().put(stock.getStockName(), amount);
-//           }
-//
-//           // Get the amount of money you have in portfolio
-//           double current_balance_portfolio = user.getPortfolio().getAccountBalance();
-//
-//           // Updating the amount left in account
-//           user.getPortfolio().setAccountBalance(current_balance_portfolio - amount_used_for_purchase);
-//
-//           //Updating transaction history
-//           Transaction transaction = new Transaction(LocalDateTime.now(), stock.getStockName(), "Buy",
-//           (amount_used_for_purchase/amount), amount);
-//           user.getTransactionHistory().add(transaction);
-//           }
-////    }
-////
-////    public void sell(Portfolio portfolio, int amount, Stock stock){
-////
-////        portfolio.getPortfolio().put(stock, portfolio.getPortfolio().get(stock) - amount);
-////
-////        double current_balance_portfolio = portfolio.getAccountBalance();
-////
-////        double amount_received = stock.getPriceHistory().getDailyPriceHistory().get(stock.getStockSymbol()) * amount;
-////
-////        portfolio.setAccountBalance(current_balance_portfolio + amount_received);
-////    }
-////    /**
-////     * Return whether a user exists with username identifier.
-////     * @param identifier the username to check.
-////     * @return whether a user exists with username identifier
-////     */
-////    @Override
-////    public boolean existsByName(String identifier) {
-////        return stocks.containsKey(identifier);
-////    }
-////}
+package data_access;
+
+import entity.*;
+import netscape.javascript.JSException;
+import netscape.javascript.JSObject;
+import use_case.buy.BuyDataAccessInterface;
+import use_case.searching.SearchDataAccessInterface;
+import use_case.sell.SellDataAccessInterface;
+
+import java.io.*;
+import java.time.LocalDateTime;
+import java.util.*;
+
+public class StockDataAccessObject implements SellDataAccessInterface, SearchDataAccessInterface, BuyDataAccessInterface {
+    private final JSObject jsonFile;
+
+    private final Map<String, Integer> headers = new LinkedHashMap<>();
+
+    private final Map<String, Stock> stocks = new HashMap<>();
+
+    public StockDataAccessObject(String JsonPath) throws IOException {
+
+        jsonFile = new JSObject() {
+            @Override
+            public Object call(String methodName, Object... args) throws JSException {
+                return null;
+            }
+
+            @Override
+            public Object eval(String s) throws JSException {
+                return null;
+            }
+
+            @Override
+            public Object getMember(String name) throws JSException {
+                return null;
+            }
+
+            @Override
+            public void setMember(String name, Object value) throws JSException {
+
+            }
+
+            @Override
+            public void removeMember(String name) throws JSException {
+
+            }
+
+            @Override
+            public Object getSlot(int index) throws JSException {
+                return null;
+            }
+
+            @Override
+            public void setSlot(int index, Object value) throws JSException {
+
+            }
+        };
+        //change
+        headers.put("stockSymbol", 0);
+        headers.put("priceHistory", 1);
+
+        if (jsonFile.length() == 0) {
+            save();
+        } else {
+
+            try (BufferedReader reader = new BufferedReader(new FileReader(jsonFile))) {
+                String header = reader.readLine();
+
+                // For later: clean this up by creating a new Exception subclass and handling it in the UI.
+                assert header.equals("username,password,creation_time");
+
+                String row;
+                while ((row = reader.readLine()) != null) {
+                    String[] col = row.split(",");
+                    String stockSymbol = String.valueOf(col[headers.get("username")]);
+                    PriceHistory priceHistory = None; //change!
+                    Stock stock = stockFactory.create(stockSymbol, priceHistory);
+                    stocks.put(stockSymbol, stock);
+                }
+            }
+        }
+    }
+
+    public StockDataAccessObject(File jsonFile) {
+        this.jsonFile = jsonFile;
+    }
+
+
+    public void save(Stock stock) {
+        stocks.put(stock.getStockSymbol(), stock);
+        this.save();
+    }
+
+
+    public Stock getStockObject(String stockSymbol) {
+        return stocks.get(stockSymbol);
+    }
+
+    private void save() {
+        BufferedWriter writer;
+        try {
+            writer = new BufferedWriter(new FileWriter(jsonFile));
+            writer.write(String.join(",", headers.keySet()));
+            writer.newLine();
+
+            for (Stock stock : stocks.values()) {
+                String line = String.format("%s,%s",
+                        stock.getStockSymbol(), stock.getPriceHistory());
+                writer.write(line);
+                writer.newLine();
+            }
+
+            writer.close();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    /**
+     * Return whether a user exists with username identifier.
+     * @param identifier the username to check.
+     * @return whether a user exists with username identifier
+     */
+    @Override
+    public boolean existsByName(String identifier) {
+        return stocks.containsKey(identifier);
+    }
+}
--- src/use_case/buy/BuyInteractor.java
+++ src/use_case/buy/BuyInteractor.java
@@ -3,6 +3,7 @@ package use_case.buy;
 import entity.CommonUser;
 import entity.Portfolio;
 import entity.Stock;
+import entity.Transaction;
 
 import java.time.LocalDateTime;
 
@@ -27,17 +28,33 @@ public class BuyInteractor implements BuyInputBoundary{
     public void buy(BuyInputData buyInputData) {
 
             LocalDateTime now = LocalDateTime.now();
-
             Portfolio portfolio = commonUser.getPortfolio();
             // If the account has enough money to buy, then they can buy
-            double amount_used_for_purchase = buyInputData.getAmount() * stock.getPriceHistory().
+        int amount = buyInputData.getAmount();
+        double amount_used_for_purchase = amount * stock.getPriceHistory().
                     getDailyPriceHistory().get(String.valueOf(now.getDayOfMonth()));
-            if (portfolio.getAccountBalance() >= amount_used_for_purchase) {
-                buyDataAccessObject.buy(buyInputData.getAmount(), stock, commonUser, amount_used_for_purchase);
 
-                BuyOutputData buyOutputData = new BuyOutputData(stock.getStockName(), now.toString());
-                buyPresenter.prepareSuccessView(buyOutputData);
+        if (portfolio.getAccountBalance() >= amount_used_for_purchase) {
+            if (commonUser.getPortfolio().getPortfolio().containsKey(stock.getStockSymbol())) {
+                int old_value = commonUser.getPortfolio().getPortfolio().get(stock.getStockSymbol());
+                int new_value = old_value + amount;
+                commonUser.getPortfolio().getPortfolio().replace(stock.getStockSymbol(), old_value, new_value);
             } else {
+                commonUser.getPortfolio().getPortfolio().put(stock.getStockName(), amount);
+            }
+            // Get the amount of money you have in portfolio
+            double current_balance_portfolio = commonUser.getPortfolio().getAccountBalance();
+
+            // Updating the amount left in account
+            commonUser.getPortfolio().setAccountBalance(current_balance_portfolio - amount_used_for_purchase);
+
+            //Updating transaction history
+            Transaction transaction = new Transaction(LocalDateTime.now(), stock.getStockName(), "Buy",
+                    (amount_used_for_purchase / amount), amount);
+            commonUser.getTransactionHistory().add(transaction);
+            BuyOutputData buyOutputData = new BuyOutputData(stock.getStockName(), now.toString());
+            buyPresenter.prepareSuccessView(buyOutputData);
+        } else {
                 int affordable_amount = buyInputData.getAmount();
                 double price_for_stock = stock.getPriceHistory().getDailyPriceHistory().
                         get(String.valueOf(now.getDayOfMonth()));

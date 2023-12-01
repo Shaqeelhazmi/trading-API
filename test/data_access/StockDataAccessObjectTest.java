4b9fc104a797f0ec7e6013a88ba93497f9823603
matkuang <matthew.kuang@mail.utoronto.ca>
2023-12-01 17:05:30 -0500

    added sdao test

--- test/data_access/StockDataAccessObjectTest.java
+++ test/data_access/StockDataAccessObjectTest.java
@@ -1,4 +1,42 @@
-import static org.junit.jupiter.api.Assertions.*;
+package data_access;
+
+import entity.*;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.HashMap;
+
 class StockDataAccessObjectTest {
-  
+
+    @Test
+    void existsByName() {
+    }
+    @Test
+    void save() {
+        StockDataAccessObject sdao;
+        try {
+            sdao = new StockDataAccessObject("./stocks.json");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        HashMap<String, Double> dailyPriceHistory = new HashMap<>();
+        HashMap<String, Double> weeklyPriceHistory = new HashMap<>();
+        HashMap<String, Double> monthlyPriceHistory = new HashMap<>();
+        dailyPriceHistory.put("2023-11-30", 156.95);
+        weeklyPriceHistory.put("2023-11-30", 156.95);
+        monthlyPriceHistory.put("2023-11-30", 156.95);
+
+
+        PriceHistory testPriceHistory = new PriceHistory(
+                dailyPriceHistory,
+                weeklyPriceHistory,
+                monthlyPriceHistory
+        );
+        Stock testStock = new Stock("TSLA", "Tesla", testPriceHistory);
+        sdao.save(testStock);
+    }
+    @Test
+    void getStock() {
+    }
 }

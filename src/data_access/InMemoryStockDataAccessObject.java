7ac3eefde04700e27aa30e182299c382e3d1e96f
muratb1lg1n <muratb2003@gmail.com>
2023-12-02 13:10:30 -0500

    old method deleted

--- src/data_access/InMemoryStockDataAccessObject.java
+++ src/data_access/InMemoryStockDataAccessObject.java
@@ -40,29 +40,3 @@ public class InMemoryStockDataAccessObject implements BuyDataAccessInterface, Se
     }
 
 }
-
-
-
-//    @Override
-//    public void sell(int amount, Stock stock, CommonUser user, double amount_received) {
-//        // Change the amount the user have of that stock in portfolio
-//        if (user.getPortfolio().getPortfolio().get(stock.getStockSymbol()) == amount) {
-//            user.getPortfolio().getPortfolio().remove(stock.getStockSymbol(), amount);
-//        } else {
-//            int old_value = user.getPortfolio().getPortfolio().get(stock.getStockSymbol());
-//            int new_value = old_value - amount;
-//            user.getPortfolio().getPortfolio().replace(stock.getStockSymbol(),
-//                    old_value, new_value);
-//        }
-//
-//        // Get the amount of money you have in portfolio
-//        double current_balance_portfolio = user.getPortfolio().getAccountBalance();
-//
-//        // Updating the amount left in account
-//        user.getPortfolio().setAccountBalance(current_balance_portfolio + amount_received);
-//
-//        Transaction transaction = new Transaction(LocalDateTime.now(), stock.getStockName(), "Sell",
-//                (amount_received/amount), amount);
-//        user.getTransactionHistory().add(transaction);
-//    }
-

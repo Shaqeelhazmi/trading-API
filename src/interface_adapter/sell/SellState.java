1966e0bf7dc3e276c7fa68c9dc2f2d47f4185616
muratb1lg1n <muratb2003@gmail.com>
2023-12-02 12:52:46 -0500

    sell view updated

--- src/interface_adapter/sell/SellState.java
+++ src/interface_adapter/sell/SellState.java
@@ -24,6 +24,8 @@ public class SellState {
     public void setStockSymbol(String stockSymbol){this.stockSymbol = stockSymbol;}
     public String getStockName(){return stockName;}
     public void setStockName(String stockName) {this.stockName = stockName;}
+    public String getUsername(){return username;}
+    public void setUsername(String username) {this.username = username;}
 
     @Override
     public String toString(){
--- src/view/SellView.java
+++ src/view/SellView.java
@@ -3,6 +3,7 @@ package view;
 import interface_adapter.sell.SellController;
 import interface_adapter.sell.SellState;
 import interface_adapter.sell.SellViewModel;
+import interface_adapter.logged_in.LoggedInViewModel;
 
 import javax.swing.*;
 import java.awt.*;
@@ -12,25 +13,31 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
+import java.io.IOException;
+
 public class SellView extends JPanel implements ActionListener, PropertyChangeListener{
     public final String viewName = "sell";
     private final SellViewModel sellViewModel;
-    private final JTextField stockInputField = new JTextField(15);
+    private final LoggedInViewModel loggedInViewModel;
     private final JTextField amountInputField = new JTextField(15);
     private final SellController sellController;
     private final JButton sell;
+    private String stockSymbol;
+    private String stockName;
 
-    public SellView(SellController controller, SellViewModel sellViewModel){
+    public SellView(SellController controller, SellViewModel sellViewModel, LoggedInViewModel loggedInViewModel){
         this.sellController = controller;
         this.sellViewModel = sellViewModel;
+        this.loggedInViewModel = loggedInViewModel;
+
+        String stockSymbol = sellViewModel.getSellState().getStockSymbol();
+        String stockName = sellViewModel.getSellState().getStockName();
+
         sellViewModel.addPropertyChangeListener(this);
 
         JLabel title = new JLabel(sellViewModel.TITLE_LABEL);
         title.setAlignmentX(Component.CENTER_ALIGNMENT);
 
-        // Cannot resolve LabelTextPanel for an unknown reason
-        LabelTextPanel  stockInfo = new LabelTextPanel(
-                new JLabel(sellViewModel.STOCK_LABEL), stockInputField);
         LabelTextPanel  amountInfo = new LabelTextPanel(
                 new JLabel(sellViewModel.AMOUNT_LABEL), amountInputField);
 
@@ -42,20 +49,32 @@ public class SellView extends JPanel implements ActionListener, PropertyChangeLi
         sell.addActionListener(
                 new ActionListener() {
                     @Override
-                    public void actionPerformed(ActionEvent e) {
-                        if (e.getSource().equals(sell)){
-                            // Need to figure out how to match the given string to Stock object
-                            sellController.sell(stockInputField.getText(), Integer.parseInt(amountInputField.getText()));
+                    public void actionPerformed(ActionEvent evt) {
+                        if (evt.getSource().equals(sell)){
+                            SellState currentState = sellViewModel.getSellState();
+                            try {
+                                sellController.sell(stockSymbol, currentState.getAmount(), currentState.getUsername());
+                            } catch (IOException e) {
+                                throw new RuntimeException(e);
+                            }
                         }
                     }
                 }
         );
 
-        stockInputField.addKeyListener(
+        amountInputField.addKeyListener(
                 new KeyListener() {
                     @Override
                     public void keyTyped(KeyEvent e) {
-                        SellState currentState = sellViewModel.getSellState();
+                        char c = e.getKeyChar();
+                        if (!Character.isDigit(c)){
+                            e.consume();
+                        } else {
+                            SellState currentState = sellViewModel.getSellState();
+                            int amount = Integer.parseInt(amountInputField.getText() + e.getKeyChar());
+                            currentState.setAmount(amount);
+                            sellViewModel.setState(currentState);
+                        }
 
                     }
 
@@ -71,20 +90,16 @@ public class SellView extends JPanel implements ActionListener, PropertyChangeLi
         this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
         this.add(title);
-        this.add(stockInfo);
         this.add(amountInfo);
         this.add(buttons);
     }
     public void actionPerformed(ActionEvent e) {
-        System.out.println("Cancel not implemented yet.");
+
     }
 
     @Override
     public void propertyChange(PropertyChangeEvent evt) {
-        SellState state = (SellState) evt.getNewValue();
-        if (state.getSellError() != null) {
-            JOptionPane.showMessageDialog(this, state.getSellError());
-        }
+
     }
 
 }

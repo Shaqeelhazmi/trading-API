a25a6a716ceec2a7bb358cd5ae4834a50b8af17c
muratb1lg1n <muratb2003@gmail.com>
2023-12-03 17:03:31 -0500

    portfolio use case factory created

--- src/app/PortfolioUseCaseFactory.java
+++ src/app/PortfolioUseCaseFactory.java
@@ -1,4 +1,36 @@
 package app;
 
+import interface_adapter.ViewManagerModel;
+import interface_adapter.logged_in.LoggedInViewModel;
+import interface_adapter.portfolio.PortfolioController;
+import interface_adapter.portfolio.PortfolioPresenter;
+import interface_adapter.portfolio.PortfolioViewModel;
+import use_case.portfolio.PortfolioDataAccessInterface;
+import use_case.portfolio.PortfolioInputBoundary;
+import use_case.portfolio.PortfolioInteractor;
+import use_case.portfolio.PortfolioOutputBoundary;
+import view.PortfolioView;
+
+import javax.swing.*;
+import java.io.IOException;
+
 public class PortfolioUseCaseFactory {
+    public static PortfolioView create(ViewManagerModel viewManagerModel, PortfolioViewModel portfolioViewModel,
+                                       LoggedInViewModel loggedInViewModel, PortfolioDataAccessInterface portfolioDataAccessObject) {
+        try {
+            PortfolioController portfolioController =  createPortfolioUseCase(viewManagerModel, portfolioViewModel, portfolioDataAccessObject);
+            return new PortfolioView(portfolioController, portfolioViewModel, viewManagerModel, loggedInViewModel);
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(null, "Could not open file");
+        }
+        return null;
+    }
+
+    private static PortfolioController createPortfolioUseCase(ViewManagerModel viewManagerModel, PortfolioViewModel portfolioViewModel,
+                                                  PortfolioDataAccessInterface portfolioDataAccessObject) throws IOException{
+        PortfolioOutputBoundary portfolioOutputBoundary = new PortfolioPresenter(viewManagerModel, portfolioViewModel);
+        PortfolioInputBoundary portfolioInteractor = new PortfolioInteractor(portfolioDataAccessObject, portfolioOutputBoundary);
+
+        return new PortfolioController(portfolioInteractor);
+    }
 }
--- src/view/PortfolioView.java
+++ src/view/PortfolioView.java
@@ -1,4 +1,72 @@
 package view;
 
-public class PortfolioView {
+import interface_adapter.ViewManagerModel;
+import interface_adapter.logged_in.LoggedInViewModel;
+import interface_adapter.portfolio.PortfolioController;
+import interface_adapter.portfolio.PortfolioState;
+import interface_adapter.portfolio.PortfolioViewModel;
+
+import interface_adapter.searching.SearchViewModel;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
+
+public class PortfolioView extends JPanel implements ActionListener, PropertyChangeListener {
+    public final String viewName = "View Portfolio";
+    private final PortfolioController portfolioController;
+
+    private final PortfolioViewModel portfolioViewModel;
+
+    private final JButton goBack;
+
+    public PortfolioView(PortfolioController portfolioController, PortfolioViewModel portfolioViewModel,
+                         ViewManagerModel viewManagerModel, LoggedInViewModel loggedInViewModel) {
+        this.portfolioController = portfolioController;
+        this.portfolioViewModel = portfolioViewModel;
+        portfolioViewModel.addPropertyChangeListener(this);
+
+        JPanel buttons = new JPanel();
+        goBack = new JButton(SearchViewModel.GOBACK);
+        buttons.add(goBack);
+        buttons.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+
+        goBack.addActionListener(
+                new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        if (e.getSource().equals(goBack)){
+                            viewManagerModel.setActiveView(loggedInViewModel.getViewName());
+                            viewManagerModel.firePropertyChanged();
+                        }
+                    }
+                }
+        );
+
+        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+
+        this.add(buttons);
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {System.out.println("Click" + e.getActionCommand());}
+
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+        if (evt.getSource().equals(portfolioViewModel)) {
+            PortfolioState state = (PortfolioState) evt.getNewValue();
+
+            if (state.getError() != null) {
+                JOptionPane.showMessageDialog(this, state.getError());
+            }
+        }
+    }
 }

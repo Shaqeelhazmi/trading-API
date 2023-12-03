261658a1896b42680ba286c4d703e07a58703484
Ruochen Feng <ruochen.feng@mail.utoronto.ca>
2023-12-03 15:10:43 -0500

    PriceHistoryInterfaceTest refines

--- test/entity/PriceHistoryInterfaceTest.java
+++ test/entity/PriceHistoryInterfaceTest.java
@@ -15,30 +15,24 @@ class PriceHistoryInterfaceTest {
         HashMap<String, Double> dailyPriceHistory = new HashMap<>();
         HashMap<String, Double> weeklyPriceHistory = new HashMap<>();
         HashMap<String, Double> monthlyPriceHistory = new HashMap<>();
-        dailyPriceHistory.put("2023-11-10", 130.0);
-        weeklyPriceHistory.put("2020-08-07", 1491.0);
-        monthlyPriceHistory.put("2014-12-31", 545.0);
         priceHistory = new PriceHistory(dailyPriceHistory, weeklyPriceHistory, monthlyPriceHistory);
     }
 
     @Test
     void getDailyPriceHistory() {
         HashMap<String, Double> dailyPriceHistory = new HashMap<>();
-        dailyPriceHistory.put("2023-11-10", 130.0);
         assertEquals(dailyPriceHistory, priceHistory.getDailyPriceHistory());
     }
 
     @Test
     void getWeeklyPriceHistory() {
         HashMap<String, Double> weeklyPriceHistory = new HashMap<>();
-        weeklyPriceHistory.put("2020-08-07", 1491.0);
         assertEquals(weeklyPriceHistory, priceHistory.getWeeklyPriceHistory());
     }
 
     @Test
     void getMonthlyPriceHistory() {
         HashMap<String, Double> monthlyPriceHistory = new HashMap<>();
-        monthlyPriceHistory.put("2014-12-31", 545.0);
         assertEquals(monthlyPriceHistory, priceHistory.getMonthlyPriceHistory());
     }
 }
